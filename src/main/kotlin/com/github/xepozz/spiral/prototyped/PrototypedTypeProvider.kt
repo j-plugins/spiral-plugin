c3267367b7b5927d3e70d87d8e9e21a0fb49b342
Dmitriy Derepko <xepozz@list.ru>
2025-09-30 20:54:16 +0400

    refactor: optimize prototypes type provider

--- src/main/kotlin/com/github/xepozz/spiral/prototyped/PrototypedTypeProvider.kt
+++ src/main/kotlin/com/github/xepozz/spiral/prototyped/PrototypedTypeProvider.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.jetbrains.php.PhpIndex
+import com.jetbrains.php.lang.PhpLangUtil
 import com.jetbrains.php.lang.psi.PhpPsiUtil
 import com.jetbrains.php.lang.psi.elements.FieldReference
 import com.jetbrains.php.lang.psi.elements.PhpClass
@@ -25,11 +26,10 @@ class PrototypedTypeProvider : PhpTypeProvider4 {
         if (DumbService.isDumb(project)) return null
 
         val element = element as? FieldReference ?: return null
-        val variable = element.classReference as? Variable ?: return null
-        if (variable.name != "this") return null
+        if (!PhpLangUtil.isThisReference(element.classReference)) return null
 
-        val phpClass = PsiTreeUtil.getParentOfType(element, PhpClass::class.java) ?: return null
-        if (!phpClass.hasTrait(SpiralFrameworkClasses.PROTOTYPE_TRAIT)) return null
+        val currentPhpClass = PsiTreeUtil.getParentOfType(element, PhpClass::class.java) ?: return null
+        if (!currentPhpClass.hasTrait(SpiralFrameworkClasses.PROTOTYPE_TRAIT)) return null
 
         val fieldName = element.name ?: return null
 
@@ -37,36 +37,28 @@ class PrototypedTypeProvider : PhpTypeProvider4 {
 
         if (fieldName !in prototypes) return null
 
-//        println("prototypes: ${prototypes}, fieldName: $fieldName")
-        return getBySignature(fieldName, emptySet(), 0, project)?.firstOrNull()?.type
-//        return PhpType().add("#${key}$fieldName")
+        val prototypeFqn = PrototypedIndex.getPrototypeClass(fieldName, project) ?: return null
+
+        val phpIndex = PhpIndex.getInstance(project)
+
+        val classes = phpIndex.getAnyByFQN(prototypeFqn)
+
+        val phpClass = classes.firstOrNull() ?: return null
+
+//        println("prototype fieldName: $fieldName class: $phpClass")
+
+        return PhpType().add(phpClass.fqn)
     }
 
     override fun complete(
         signature: String,
         project: Project,
-    ): PhpType? {
-//        println("complete: $signature")
-        val prototypeName = signature.substringAfter(key)
-
-        return getBySignature(prototypeName, emptySet(), 0, project)?.firstOrNull()?.type
-    }
+    ) = null
 
     override fun getBySignature(
         expression: String,
         visited: Set<String>,
         depth: Int,
         project: Project,
-    ): Collection<PhpNamedElement>? {
-//        println("getBySignature: $expression")
-
-        val prototypeFqn = PrototypedIndex.getPrototypeClass(expression, project)
-        val phpIndex = PhpIndex.getInstance(project)
-        val classes = phpIndex.getAnyByFQN(prototypeFqn)
-
-//        println("find classes $classes by prototypeFqn $prototypeFqn")
-        if (classes.isEmpty()) return null
-
-        return classes
-    }
+    ) = null
 }
